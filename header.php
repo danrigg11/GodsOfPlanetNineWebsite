c5524256272e61b7988e6d4e109b5a4d6ed8cdb2
Daniel <dan_rigg11@hotmail.com>
2018-08-20 17:00:17 +0100

    Created rules page

--- header.php
+++ header.php
@@ -10,17 +10,17 @@
   <body>
 
     <nav class="navbar navbar-expand-xs navbar-light bg-light">
-    <a class="navbar-brand" href="#"><h1>Gods of Planet Nine</h1></a>
+    <a class="navbar-brand" href="index.php"><h1>Gods of Planet Nine</h1></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav">
-        <li class="nav-item active">
-          <a class="nav-link" href="#"><h3>Home</h3></a>
+        <li class="nav-item">
+          <a class="nav-link" href="index.php"><h3>Home</h3></a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#"><h3>Rules</h3></a>
+          <a class="nav-link" href="rules.php"><h3>Rules</h3></a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#"><h3>Current Score</h3></a>
