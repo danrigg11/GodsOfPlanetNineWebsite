22400be49413f03bdc52828f5c8ed71f37d9585d
Daniel <dan_rigg11@hotmail.com>
2018-08-20 13:43:41 +0100

    linked js & css. created navbar

--- Index.php
+++ Index.php
@@ -1,1 +1,20 @@
+<?php include 'header.php' ?>
 
+<div class="row">
+
+<div class="col-4">
+  <h3 class="text-center">Rules</h3>
+</div>
+
+<div class="col-4">
+  <h3 class="text-center">Countdown</h3>
+</div>
+
+<div class="col-4">
+  <h3 class="text-center">Current Score</h3>
+</div>
+
+</div>
+
+
+<?php include 'footer.php' ?>
--- header.php
+++ header.php
@@ -3,5 +3,29 @@
   <head>
     <meta charset="utf-8">
     <title></title>
+
+    <link rel="stylesheet" href="/css/bootstrap.css">
+    <link rel="stylesheet" href="/css/style.css">
   </head>
   <body>
+
+    <nav class="navbar navbar-expand-xs navbar-light bg-light">
+    <a class="navbar-brand" href="#">Gods of Planet Nine</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Rules</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Current Score</a>
+        </li>
+
+      </ul>
+    </div>
+  </nav>
