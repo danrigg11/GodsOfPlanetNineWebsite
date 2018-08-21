9264337057fcef23c060d9be3b58d013ef14bb40
Daniel <dan_rigg11@hotmail.com>
2018-08-21 12:40:49 +0100

    changed landing page

--- Index.php
+++ Index.php
@@ -1,50 +1,100 @@
 <?php include 'header.php' ?>
 
-<div class="row align-items-center bg-dark">
-  <div class="col-sm-12 col-md-4">
-    <h3 class="text-center text-white">Rules</h3>
-  </div>
+<div class="card-deck mb-3 pl-3 pr-3 text-center">
+        <div class="card mb-4 shadow-sm">
+          <div class="card-header">
+            <h4 class="my-0 font-weight-normal">Game Rules</h4>
+          </div>
+          <div class="card-body">
+            <h1 class="card-title pricing-card-title">Rulebook</h1>
+            <h2 class="card-title pricing-card-title mb-3">Copy <small class="text-muted"> 1.1</small></h2>
+            <button type="button" class="btn btn-lg btn-block btn-outline-dark">Before You Start</button>
+          </div>
+        </div>
+        <div class="card mb-4 shadow-sm">
+          <div class="card-header">
+            <h4 class="my-0 font-weight-normal">Countdown</h4>
+          </div>
+          <div class="card-body">
+            <h1 class="card-title pricing-card-title">Season 1.2</h1>
+            <!-- Display the countdown timer in an element -->
+            <h2 id="demo" class="text-center mb-3"></h2>
+            <button type="button" class="btn btn-lg btn-block btn-dark">See Whats Next</button>
+          </div>
+        </div>
+        <div class="card mb-4 shadow-sm">
+          <div class="card-header">
+            <h4 class="my-0 font-weight-normal">Current Score</h4>
+          </div>
+          <div class="card-body">
+            <h1 class="card-title pricing-card-title">TigerBoy32</h1>
+            <h2 class="card-title pricing-card-title mb-3">32:45 <small class="text-muted">Seconds</small></h2>
+            <button type="button" class="btn btn-lg btn-block btn-outline-dark">Sign up for free</button>
+          </div>
+        </div>
+      </div>
 
-  <div class="col-sm-12 col-md-4">
-    <h3 class="text-center text-white">Countdown</h3>
-    <!-- Display the countdown timer in an element -->
-    <h3 id="demo" class="text-center text-white"></h3>
-  </div>
 
-  <div class="col-sm-12 col-md-4">
-    <h3 class="text-center text-white">Current Score</h3>
-  </div>
-</div>
+
+
+
+
 
 <div class="row HomeScreenBar mb-5">
-<div class="col-4 bg-dark">
-</div>
-<div class="col-4">
-<h3 class="text-center font-weight-bold">Previous Levels</h3>
-</div>
-<div class="col-4 bg-dark">
-</div>
-</div>
-
-<div class="container">
-
-<div class="row">
-
-<div class="col-4 mb-3 mt-3">
-  <div class="card">
-    <div class="card-header">
-      <h5 class="card-title">Title</h5>
-    </div>
-    <div class="card-body">
-      <img class="card-img-top" src="Images/Screen Shot 2018-08-19 at 21.47.57.png" alt="Version Image">
-      <p class="card-text">Version description.</p>
-      <a href="#" class="btn btn-outline-dark">Go to version</a>
-    </div>
+  <div class="col-4 bg-dark">
+  </div>
+  <div class="col-4">
+    <h3 class="text-center font-weight-bold">Previous Levels</h3>
+  </div>
+  <div class="col-4 bg-dark">
   </div>
 </div>
 
-</div>
-</div>
+
+
+  <div class="row pl-4 pr-4">
+
+    <div class="col-4 mb-3 mt-3">
+      <div class="card">
+        <div class="card-header">
+          <h5 class="card-title">Title</h5>
+        </div>
+        <div class="card-body">
+          <img class="card-img-top" src="Images/Screen Shot 2018-08-19 at 21.47.57.png" alt="Version Image">
+          <p class="card-text">Version description.</p>
+          <a href="#" class="btn btn-outline-dark">Go to version</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-4 mb-3 mt-3">
+      <div class="card">
+        <div class="card-header">
+          <h5 class="card-title">Title</h5>
+        </div>
+        <div class="card-body">
+          <img class="card-img-top" src="Images/Screen Shot 2018-08-19 at 21.47.57.png" alt="Version Image">
+          <p class="card-text">Version description.</p>
+          <a href="#" class="btn btn-outline-dark">Go to version</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-4 mb-3 mt-3">
+      <div class="card">
+        <div class="card-header">
+          <h5 class="card-title">Title</h5>
+        </div>
+        <div class="card-body">
+          <img class="card-img-top" src="Images/Screen Shot 2018-08-19 at 21.47.57.png" alt="Version Image">
+          <p class="card-text">Version description.</p>
+          <a href="#" class="btn btn-outline-dark">Go to version</a>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
 
 
 <?php include 'footer.php' ?>
